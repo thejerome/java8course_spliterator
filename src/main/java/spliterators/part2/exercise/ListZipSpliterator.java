dc2f4a23724343afe499411b4bf5c865744c0f4a
Mikhail Chtetsov <chtetsoff@gmail.com>
2018-02-09 01:32:27 +0300

    part 2

--- src/main/java/spliterators/part2/exercise/ListZipSpliterator.java
+++ src/main/java/spliterators/part2/exercise/ListZipSpliterator.java
@@ -5,32 +5,54 @@ import java.util.Spliterator;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
-public class ListZipSpliterator<L, R, T>  implements Spliterator<T> {
+public class ListZipSpliterator<L, R, T> implements Spliterator<T> {
+
+    private final List<L> list1;
+    private final List<R> list2;
+    private final BiFunction<L, R, T> combiner;
+    private int startInclusive;
+    private int endExclusive;
+
     public ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner) {
+        this(list1, list2, combiner, 0, Math.min(list1.size(), list2.size()));
+    }
+
+    public ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner,
+                              int startInclusive, int endExclusive) {
+        this.list1 = list1;
+        this.list2 = list2;
+        this.combiner = combiner;
+        this.startInclusive = startInclusive;
+        this.endExclusive = endExclusive;
 
     }
 
     @Override
     public boolean tryAdvance(Consumer<? super T> action) {
-        //TODO
-        throw new UnsupportedOperationException();
+        if (startInclusive < endExclusive) {
+            action.accept(combiner.apply(list1.get(startInclusive), list2.get(startInclusive)));
+            startInclusive++;
+            return true;
+        }
+        return false;
     }
 
     @Override
     public Spliterator<T> trySplit() {
-        //TODO
-        throw new UnsupportedOperationException();
+        int mid = startInclusive + (endExclusive - startInclusive) / 2;
+
+        ListZipSpliterator listZipSpliterator = new ListZipSpliterator<>(list1, list2, combiner, startInclusive, mid);
+        startInclusive = mid;
+        return listZipSpliterator;
     }
 
     @Override
     public long estimateSize() {
-        //TODO
-        throw new UnsupportedOperationException();
+        return endExclusive - startInclusive;
     }
 
     @Override
     public int characteristics() {
-        //TODO
-        throw new UnsupportedOperationException();
+        return 0;
     }
 }
