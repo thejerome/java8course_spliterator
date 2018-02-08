507fe183751c8df1443ebd8c05ee198f3a5efeca
Konstantin Mavropulo <konstantin.n.mavropulo@gmail.com>
2018-02-08 23:52:14 +0300

    part2_Mavropulo_Konstantin: part2 has been completed.

--- src/main/java/spliterators/part2/exercise/ListZipSpliterator.java
+++ src/main/java/spliterators/part2/exercise/ListZipSpliterator.java
@@ -4,33 +4,53 @@ import java.util.List;
 import java.util.Spliterator;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class ListZipSpliterator<L, R, T>  implements Spliterator<T> {
+public class ListZipSpliterator<L, R, T> implements Spliterator<T> {
+    private List<T> initValues;
+    private int startVal = 0;
+    private int endVal;
+
     public ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner) {
+        initValues = IntStream.range(0, Math.min(list1.size(), list2.size())).mapToObj(i ->
+                combiner.apply(list1.get(i), list2.get(i))).collect(Collectors.toList());
+        endVal = initValues.size();
+    }
 
+    public ListZipSpliterator(List<T> initValues, int startVal, int endVal) {
+        this.initValues = initValues;
+        this.startVal = startVal;
+        this.endVal = endVal;
     }
 
     @Override
     public boolean tryAdvance(Consumer<? super T> action) {
-        //TODO
-        throw new UnsupportedOperationException();
+        if (startVal < endVal) {
+            action.accept(initValues.get(startVal));
+            startVal++;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public Spliterator<T> trySplit() {
-        //TODO
-        throw new UnsupportedOperationException();
+        int mid = (initValues.size() + startVal) / 2;
+        ListZipSpliterator<L, R, T> lrtListZipSpliterator = new ListZipSpliterator<>(
+                initValues, startVal, mid);
+        this.startVal = mid;
+        return lrtListZipSpliterator;
     }
 
     @Override
     public long estimateSize() {
-        //TODO
-        throw new UnsupportedOperationException();
+        return endVal - startVal;
     }
 
     @Override
     public int characteristics() {
-        //TODO
-        throw new UnsupportedOperationException();
+        return initValues.spliterator().characteristics();
     }
 }
