a7608514d9b5b26f66bbb85d0f98c44b5fe111f6
Vladimir Shefer <Vladimir_Shefer@epam.com>
2018-02-09 18:20:55 +0300

    part2_vladimir_shefer

--- src/main/java/spliterators/part2/exercise/ListZipSpliterator.java
+++ src/main/java/spliterators/part2/exercise/ListZipSpliterator.java
@@ -4,33 +4,79 @@ import java.util.List;
 import java.util.Spliterator;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.stream.Collector;
 
-public class ListZipSpliterator<L, R, T>  implements Spliterator<T> {
+public class ListZipSpliterator<L, R, T> implements Spliterator<T> {
+
+    List<L> list1;
+    List<R> list2;
+    BiFunction<L, R, T> combiner;
+
+    int startInclusive;
+    int position;
+    int endExclusive;
+
     public ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner) {
+        this(list1, list2, combiner, 0, Math.min(list1.size(), list2.size()));
+    }
 
+    public ListZipSpliterator(List<L> list1, List<R> list2, BiFunction<L, R, T> combiner, int startInclusive, int endExclusive) {
+        this.list1 = list1;
+        this.list2 = list2;
+        this.combiner = combiner;
+        this.startInclusive = startInclusive;
+        this.endExclusive = endExclusive;
+        this.position = startInclusive;
     }
 
     @Override
     public boolean tryAdvance(Consumer<? super T> action) {
+        if (position >= endExclusive) return false;
+        action.accept(
+                combiner.apply(
+                        list1.get(position),
+                        list2.get(position)));
+
+        position++;
+
+        return true;
         //TODO
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
     }
 
     @Override
     public Spliterator<T> trySplit() {
+        int itemsLeft = endExclusive - position;
+        if (itemsLeft <= 1) return null;
+
+        int delegatedItemsCount = itemsLeft / 2;
+
+        ListZipSpliterator subspliterator = new ListZipSpliterator<>(
+                list1,
+                list2,
+                combiner,
+                startInclusive,
+                startInclusive + delegatedItemsCount);
+
+        this.position += delegatedItemsCount;
+
+        return subspliterator;
+
         //TODO
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
     }
 
     @Override
     public long estimateSize() {
+        return endExclusive - position;
         //TODO
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
     }
 
     @Override
     public int characteristics() {
+        return Spliterator.SIZED | Spliterator.SUBSIZED | Spliterator.ORDERED;
         //TODO
-        throw new UnsupportedOperationException();
+        // throw new UnsupportedOperationException();
     }
 }
